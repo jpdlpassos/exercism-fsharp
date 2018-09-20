c9da38014f4c4b4ae5b2d5df1ad7c4ca44a233fa
Passos <Joao.Passos@dnvgl.com>
2018-09-20 19:16:01 +0200

    Initial commit

---
+++ bob/Bob.fs
@@ -0,0 +1,20 @@
+﻿module Bob
+
+open System
+
+let (|IsQuestion|_|) (i:string) = if i.EndsWith '?' then Some IsQuestion else None
+
+let (|IsSilence|_|) (i:string) = if i.Length = 0 then Some IsSilence else None
+
+let (|IsYelling |_|) (i:string) = 
+    let letters = i |> Seq.filter(fun x -> Char.IsLetter(x))
+    if not <| Seq.isEmpty letters  && Seq.forall (fun x -> Char.IsUpper(x)) letters 
+    then Some IsYelling else None
+
+let response (input: string): string = 
+    match input.Trim() with
+        | IsSilence                 -> "Fine. Be that way!"
+        | IsYelling & IsQuestion    -> "Calm down, I know what I'm doing!"
+        | IsQuestion                -> "Sure."
+        | IsYelling                 -> "Whoa, chill out!"
+        | _                         -> "Whatever."
---
+++ leap/Leap.fs
@@ -0,0 +1,13 @@
+﻿module Leap
+
+let leapYear (year: int): bool = 
+    year % 4 = 0 && (year % 100 <> 0 || year % 400 = 0);
+
+(*  
+    let (|DivisibleBy|_|) by x = if x % by = 0 then Some DivisibleBy else None
+    match year with
+        | DivisibleBy 400 -> true
+        | DivisibleBy 100 -> false
+        | DivisibleBy 4   -> true
+        | _               -> false
+*)
